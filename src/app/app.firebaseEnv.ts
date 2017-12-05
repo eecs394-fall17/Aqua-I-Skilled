2c1df9369bb456e1ad5161bc4d350427deae5547
angelomatni2018 <angelomatni2018@u.northwestern.edu>
2017-12-05 04:35:34 +0000

    Removed firebaseEnv file and added it to gitignore

--- src/app/app.firebaseEnv.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDaZhd6nxDurrvCNuAJpIrn52jCSckY17c",
-  authDomain: "i-skilled.firebaseapp.com",
-  databaseURL: "https://i-skilled.firebaseio.com",
-  projectId: "i-skilled",
-  storageBucket: "i-skilled.appspot.com",
-  messagingSenderId: "224698285796"
-};
